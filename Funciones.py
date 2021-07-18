1d58061c09f397ab9df758f197faf98370e8df1a
Andrés Felipe Estrada Rodriguez <andresf.estrada.10@gmail.com>
2021-07-17 20:32:58 -0500

    Se le corrigió la ortografia a la documentacion de las funciones en el .py

--- Funciones.py
+++ Funciones.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import OrdinalEncoder
 sys.path.append('/home/grupodot/Documentos/Examen_globant/Purchase-Card-Transactions_GLBNT_TESTT/Data/')
 
 class Explorer():
-    """Esta clase compila las funciones de importacion y exploracion usadas en el ejercicio.
+    """Esta clase compila las funciones de importación y exploración usadas en el ejercicio.
     (salvo algunas funciones triviales o de menor modularidad)
     """
 
@@ -19,10 +19,10 @@ class Explorer():
         self.ord_enc = OrdinalEncoder()
 
     def importar_d(self, data_dir):
-        """Esta funcion importa todos los documentos usados durante el ejercicio
+        """Esta función importa todos los documentos usados durante el ejercicio
 
         Args:
-            data_dir (string): constante que se define al inicio del ejercicio con una direccion especifica del path
+            data_dir (string): constante que se define al inicio del ejercicio con una dirección especifica del path
 
         Returns:
             Pandas.DataFrame
@@ -38,7 +38,7 @@ class Explorer():
         return df
     
     def limpieza(self, df, valor_remplazo):
-        """Funcion de limpieza de nan y variable temporal.
+        """Función de limpieza de nan y variable temporal.
 
         Args:
             df (Pandas.DataFrame): DataFrame
@@ -52,16 +52,25 @@ class Explorer():
         return df_f
     
     def clean_import(self, data_dir, valor_remplazo="noreg"):
+        """Remplaza los NaN por la string "valor_remplazo"
+
+        Args:
+            data_dir (string): string con la direccion en el path para todo el conjunto de datos
+            valor_remplazo (str, optional): Valor con el que seran remplazados los NaN. Defaults to "noreg".
+
+        Returns:
+            DataFrame: Pandas DataFrame
+        """     
         df = self.importar_d(data_dir)
         df_f = self.limpieza(df,valor_remplazo)
 
         return df_f
     
     def graficador(self, df_var, size = (12,6), tipo ="plot", tittle =""):
-        """Funcion para graficas unicas de pyplot
+        """Función para gráficas unitarias de pyplot
 
         Args:
-            df_var (DataFrame de dimension 1 o array): En esta funcion se requiere que se decretre de una vez el nombre de la variable del data frame, ej: df["variable1"]
+            df_var (DataFrame de dimension 1 o array): En esta función se requiere que se decrete de una vez el nombre de la variable del DataFrame, ej: df["variable1"]
             tipo (str, optional): [description]. Defaults to "plot".
             tittle (str, optional): [description]. Defaults to "".
             size (tuple, optional): [description]. Defaults to (12,16).
@@ -80,11 +89,11 @@ class Explorer():
         plt.show()
 
     def comparador (self, variable, df1, df2, df3):
-        """Esta funcion me pervite visualizar graficamente la distribucion la misma variable en tres DataFrames diferentes.
+        """Esta función me permite visualizar gráficamente la distribución la misma variable en tres DataFrames diferentes.
         Es obligatorio que os DataFrame tengan el mismo nombre en las variables.
 
         Args:
-            variable (string): Es el nombre de la columna del datafram a comparar
+            variable (string): Es el nombre de la columna del DataFram a comparar
             df1 (DataFrame): DataFrame
             df2 (DataFrame): DataFrame
             df3 (DataFrame): DataFrame
